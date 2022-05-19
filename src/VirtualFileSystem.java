d00ca56bdfeb05dded04983175161457611a89ea
Mazen Al Sharef <mazenelsheriif@gmail.com>
2022-05-19 17:08:53 +0200

    .

--- src/VirtualFileSystem.java
+++ src/VirtualFileSystem.java
@@ -5,15 +5,16 @@ public class VirtualFileSystem {
     int systemSizeInKB;
     Allocation allocation;
     ArrayList<Boolean> state;
-    ArrayList<DirectoryFileStructures.groupOfBlocks> groupOfBlocks ;
+    ArrayList<DirectoryFileStructures.groupOfBlocks> groupsOfBlocks ;
 
     public VirtualFileSystem(Allocation _allocation, int _systemSizeInKB) {
        this.allocation=_allocation;
        this.systemSizeInKB=_systemSizeInKB;
        state = new ArrayList<Boolean>(_systemSizeInKB);
        systemRoot = new DirectoryFileStructures.Directory("systemRoot");
-       groupOfBlocks = new ArrayList<>();
-       groupOfBlocks.add(new DirectoryFileStructures.groupOfBlocks(0, systemSizeInKB - 1,true));
+       groupsOfBlocks = new ArrayList<>();
+       DirectoryFileStructures.groupOfBlocks groupOfBlocks = new DirectoryFileStructures.groupOfBlocks(0, systemSizeInKB - 1, true);
+       groupsOfBlocks.add(groupOfBlocks);
        for (int i= 0; i< _systemSizeInKB; i++)
         {
             state.add(false);
