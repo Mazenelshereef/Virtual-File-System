3bd0f97b2ee84e138a796423ae416cb2f3280468
Mazen Al Sharef <mazenelsheriif@gmail.com>
2022-05-20 22:59:06 +0200

    indexedAllocation

--- src/IndexedAllocation.java
+++ src/IndexedAllocation.java
@@ -1,10 +1,35 @@
+import java.sql.Struct;
 import java.util.ArrayList;
 
 public class IndexedAllocation implements Allocation {
+    VirtualFileSystem System;
+     class IndexBlock
+    {
+        int value;
+        ArrayList<Integer> contentsOfBlock;
+    }
+
+    public IndexedAllocation() {
+        ArrayList<Integer> arrOfIndexedBlocks = new ArrayList<Integer>();
+        ArrayList<Integer> contentsOfBlock= new ArrayList<Integer>();
+    }
 
 
     @Override
     public ArrayList<Integer> Allocate(int fileSize) {
         return null;
+    }
+
+    public IndexBlock AllocateIndexed(int fileSize) {
+        int index = System.findFirstEmpty();
+        IndexBlock indexblock = new IndexBlock();
+        indexblock.value =index;
+        for(int i=0; i<fileSize; i++)
+        {
+            int index2 = System.findFirstEmpty();
+            indexblock.contentsOfBlock.add(index2);
+        }
+        return indexblock;//return  index block that contains array of the actual blocks to be allocated.
+        // and the index of it in the storage
     }
 }
