fb019be9b6f5876dc2c267b838482661f9531182
Mazen Al Sharef <mazenelsheriif@gmail.com>
2022-05-21 10:05:09 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String type = null;
         Allocation allocation;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the type of Allocation");
+        String type = sc.next();
         switch(type)
         {
             case ("contiguous"):
-                 String contiguousPath = null;//input
-                 allocation = new ContiguousAllocation();
-                 VirtualFileSystem ContiguousSystem = new VirtualFileSystem(allocation,10);
-                 ArrayList<Integer> ContiguousArr;
+                String contiguousPath = null;//input
+                allocation = new ContiguousAllocation();
+                VirtualFileSystem ContiguousSystem = new VirtualFileSystem(allocation,10);
+                ArrayList<Integer> ContiguousArr;
+                Scanner Contig= new Scanner(System.in);
+                System.out.print("Enter the command");
+                String command = sc.next();
+                //parse into operation,path,size
+                // switch (command)
                 ////////////////////////////////////////////////case create file////////////////////////////////
                  //take path of file and its size
                 ContiguousArr=allocation.Allocate(10);
@@ -28,6 +35,11 @@ public class Main {
                  allocation = new LinkedAllocation();
                  VirtualFileSystem LinkedSystem = new VirtualFileSystem(allocation,10);
                  ArrayList<Integer> LinkedArr;
+                Scanner linked= new Scanner(System.in);    //System.in is a standard input stream
+                System.out.print("Enter the command");
+                String command2 = sc.next();
+                //parse into operation,path,size
+                // switch (command2)
                 ////////////////////////////////////////////////case create file////////////////////////////////
                  //take path of file and its size
                 LinkedArr = allocation.Allocate(10);
@@ -44,6 +56,11 @@ public class Main {
                 allocation = new IndexedAllocation();
                 VirtualFileSystem IndexedSystem = new VirtualFileSystem(allocation,10);
                 ArrayList<Integer> IndexedArr;
+                Scanner indexed= new Scanner(System.in);    //System.in is a standard input stream
+                System.out.print("Enter the command");
+                String command3 = sc.next();
+                //parse into operation,path,size
+                // switch (command)
                 ////////////////////////////////////////////////case create file////////////////////////////////
                 //take path of file and its size
                 IndexedArr=allocation.Allocate(10);
