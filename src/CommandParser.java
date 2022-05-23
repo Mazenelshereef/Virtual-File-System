3c2e51d29f1d220611ada7ac4f7f224d45ae455d
Ze-Ro0o <azeyad682@gmail.com>
2022-05-23 19:30:15 +0200

    commandParserUpdate

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -1,11 +1,15 @@
+import java.rmi.server.ExportException;
+import java.util.ArrayList;
+
 public class CommandParser {
-    private String[] allCmds = { "CreatFile", "CreateFolder", "DeleteFile", "DeleteFolder", "DispalyDiskStatus",
-            "DisplayDiskStructure" };
+    private String[] allCmds = {"CreatFile", "CreateFolder", "DeleteFile", "DeleteFolder", "DispalyDiskStatus",
+            "DisplayDiskStructure","Exit" };
 
     public CommandParser() {
     }
 
-    void parse(String command) {
+    public ArrayList<String> parse(String command) {
+        ArrayList<String> arguments = new ArrayList<>();
         int numberOfArguments = 1;
         for (int i = 0; i < command.length(); i++) {
             if (command.charAt(i) == (' ')) {
@@ -17,43 +21,54 @@ public class CommandParser {
         if (indexOfFisrstSpace == -1) {
             if (command.equals("DispalyDiskStatus")) {
                 if (numberOfArguments != 1) {
-                    System.out.println("Wrong Number Of Argument!!");
+                    throw new ArithmeticException("Wrong Number Of Argument!!");
                 } else {
+                    arguments.add(command);
 
-                    System.out.println("hehe");
                     // call the fucnction that print Disk status
                 }
             } else if (command.equals("DisplayDiskStructure")) {
                 if (numberOfArguments != 1) {
-                    System.out.println("Wrong Number Of Argument!!");
+                    throw new ArithmeticException("Wrong Number Of Argument!!");
                 } else {
-                    System.out.println("keek");
+                    arguments.add(command);
 
                     // call function print DisplayDiskStructure
                 }
+                
+            }
+            else if (command.equals("Exit"))
+            { if (numberOfArguments != 1) {
+                throw new ArithmeticException("Wrong Number Of Argument!!");
             } else {
-                System.out.println("command not available!");
+                arguments.add(command);
+
+                // call function print DisplayDiskStructure
+            }
+
+            }
+             else {
+                throw new ArithmeticException("command not available!");
             }
 
         } else {
             String cmdName = command.substring(0, indexOfFisrstSpace);
             if (cmdName.equals("CreateFile")) {
                 if (numberOfArguments != 3) {
-                    System.out.println("Wrong Number Of Argument!!");
+                    throw new ArithmeticException("Wrong Number Of Argument!!");
                 } else {
                     String pathSize = command.substring(indexOfFisrstSpace + 1);
                     int indexOfSecSpace = pathSize.indexOf(" ");
                     String path = pathSize.substring(0, indexOfSecSpace);
                     String Size = pathSize.substring(indexOfSecSpace + 1);
-                    int sizeInt = 0;
                     try {
                         int size = Integer.parseInt(Size);
-                        sizeInt = size;
                         // call Create file function
-                        System.out.println(cmdName + '\n' + path + '\n' + sizeInt + '\n');
-
+                        arguments.add(cmdName);
+                        arguments.add(path);
+                        arguments.add(Size);
                     } catch (NumberFormatException ex) {
-                        System.out.println("3rd Argument have to be integers");
+                        throw new ArithmeticException("3rd Argument have to be integers");
                     }
 
                     // call Create file function
@@ -61,42 +76,46 @@ public class CommandParser {
                 }
             } else if (cmdName.equals("DeleteFile")) {
                 if (numberOfArguments != 2) {
-                    System.out.println("Wrong Number Of Argument!!");
+                    throw new ArithmeticException("Wrong Number Of Argument!!");
                 } else {
                     String path = command.substring(indexOfFisrstSpace + 1);
                     // callFunctionThatDeleteTheFile
 
-                    System.out.println(cmdName + " " + path + '\n');
+                    arguments.add(cmdName);
+                    arguments.add(path);
                 }
             } else if (cmdName.equals("DeleteFolder")) {
                 if (numberOfArguments != 2) {
-                    System.out.println("Wrong Number Of Argument!!");
+                    throw new ArithmeticException("Wrong Number Of Argument!!");
                 } else {
                     String path = command.substring(indexOfFisrstSpace + 1);
                     // callFunctionThatDeleteTheFolder
-                    System.out.println(cmdName + " " + path + '\n');
+                    arguments.add(cmdName);
+                    arguments.add(path);
                 }
 
             } else if (cmdName.equals("CreateFolder")) {
                 if (numberOfArguments != 2) {
-                    System.out.println("Wrong Number Of Argument!!");
+                    throw new ArithmeticException("Wrong Number Of Argument!!");
                 } else {
                     String path = command.substring(indexOfFisrstSpace + 1);
                     // callFunctionThatCreateTheFolder
-                    System.out.println(cmdName + " " + path + '\n');
+                    arguments.add(cmdName);
+                    arguments.add(path);
                 }
 
             } else {
-                System.out.println("command not available!");
+                throw new ArithmeticException("command not valid!");
 
             }
 
         }
 
+    return arguments;
     }
 
     public static void main(String[] args) {
         CommandParser cmd = new CommandParser();
-        cmd.parse("CreateFile");
+        System.out.println(cmd.parse("CreateFile s ss"));
     }
 }
